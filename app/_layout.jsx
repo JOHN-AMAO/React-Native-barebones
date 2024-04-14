3ff4dcf3257e57155291a61a9b4c1c7e93ac1325
JOHN-AMAO <amaojohn767@gmail.com>
2024-04-14 22:26:00 +0100

    using stack for viewing

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
-  return <Slot />;
+  return (
+    <>
+      <Stack.Screen name='index' />
+    </>
+  );
 };
 
 export default RootLayout;
