21531cf10ddac77fd6db2836eb2d5e04a8ecc636
JOHN-AMAO <amaojohn767@gmail.com>
2024-11-10 17:15:29 -0800

    updated template- nativewind v4

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Image, StyleSheet, Platform, SafeAreaView, Text } from 'react-native';
+
+
+
+export default function HomeScreen() {
+  return (
+    <>
+    <SafeAreaView className="flex-1 justify-center items-center bg-red-500">
+        <Text className="text-white text-4xl font-bold">Hello</Text>
+      </SafeAreaView>
+    </>
+  );
+}
+
